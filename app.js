8151a7c9086428420b7f36c13d7fd50b494a725a
Kingshuk Basak <kingshuk@fusioncharts.com>
2017-07-01 19:22:51 +0530

    Incorporated logic of infinite logic for first set of results

--- app.js
+++ app.js
@@ -2,39 +2,27 @@
 function init () {
 	$(function(){
 		setup();
-		createScroll();
 		manageScroll();
+		createScrollableElem();
 	});
 
 	var storage_title, 
 		storage_date,
 		scrollElm = [],
 		nextPageToken = '',
-		updateGrid = function () {
-			var i,
-				len = scrollElm.length,
-				item,
-				data = storage_date[0];
-
-			$('#searchContent').css("visibility", "visible");
-			for (i = 0; i < len; i++) {
-				item = scrollElm[i];
-				item.find('.pic').attr('src', data[i].thumbnail);
-				item.find('.title').html(data[i].title);
-				item.find('.pic').html(data[i].publishedAt);
-			}
-
-			updateGrid = undefined;
+		maxVisibleItems,
+		lastDrawnIndex,
+		setInitialGrid = function () {
+			updateGrid(0, lastDrawnIndex);
 		};
 
-	function createScroll () {
+	function createScrollableElem () {
 		var i,
-			limit = 5;
 			sample = $('#info');
 
 		scrollElm.push(sample);
-		for (i = 1; i < limit; i++) {
-			scrollElm[i] = sample.clone().appendTo('#scroller');
+		for (i = 1; i < lastDrawnIndex; i++) {
+			scrollElm[i] = sample.clone();
 		}
 	}
 
@@ -47,23 +35,23 @@ function init () {
 	        	storage_title = [];
 	        	storage_date = [];
 				executeRequest(createRequest());
-			})
+			});
 	    });
-	};
+	}
 
 	function createRequest () {
 		return gapi.client.youtube.search.list({
             part: 'snippet',
             type: 'video',
             q: encodeURIComponent($('#searchText').val()).replace(/%20/g, '+'),
-            maxResults: 5,
+            maxResults: 20,
             order: 'viewCount',
             pageToken : nextPageToken
        }); 
-	};
+	}
 
 	function executeRequest (request) {
-		request.execute(function(response) {
+		request.execute(function (response) {
 			var result = response.result,
 				items = result.items,
 				i,
@@ -79,25 +67,66 @@ function init () {
 				obj = {
 					title : snippet.title,
 					publishedAt : new Date(snippet.publishedAt),
-					thumbnail : snippet.thumbnails.default.url
-				}
+					thumbnail : snippet.thumbnails.default.url,
+					index : i
+				};
 				tempArrDate.push(obj);
 				tempArrTitle.push(obj);
 			}
 
-			storage_date.push(tempArrDate.sort(function(a, b) {
+			storage_date.push(tempArrDate.sort(function (a, b) {
 				return a.publishedAt < b.publishedAt;
 			}));
 
-			storage_title.push(tempArrTitle.sort(function(a, b) {
+			storage_title.push(tempArrTitle.sort(function (a, b) {
 				return a.title < b.title;
 			}));
 
-			updateGrid && updateGrid();
+			setInitialGrid && setInitialGrid();
        	});
-	};
+	}
+
+	function updateGrid (start, end) {
+		var i,
+			item,
+			data = storage_date[0],
+			datum,
+			sample = $('#info');
+
+		$('#searchContent').css("visibility", "visible");
+		for (i = start; i < end; i++) {
+			datum = data[i];
+			if (!datum) {
+				return false;
+			}
+			if (!(item = scrollElm[i])) {
+				item = scrollElm[i] = sample.clone();
+			}
+			item.appendTo('#scroller');
+			item.find('.pic').attr('src', datum.thumbnail);
+			item.find('.title').html(datum.title);
+			item.find('.date').html(datum.index);
+		}
+		return true;
+	}
 
 	function manageScroll () {
+		var scroller = $('#scroller'),
+			scrollerHeight = parseInt(scroller.css('max-height')),
+			minContentHeight = parseInt($('a').css('min-height')),
+			prevScrollTop = 0,
+			elementScrolled,
+			currentScrollTop;
 
+		maxVisibleItems = Math.ceil(scrollerHeight / minContentHeight) + 2;
+		lastDrawnIndex = maxVisibleItems + 2;
+		scroller.scroll(function () {
+			currentScrollTop = $(this).scrollTop();
+			if (currentScrollTop > prevScrollTop) {
+				elementScrolled = Math.floor((currentScrollTop - prevScrollTop) / minContentHeight);
+				elementScrolled && updateGrid(lastDrawnIndex, lastDrawnIndex + 1) && lastDrawnIndex++;
+			}
+			elementScrolled && (prevScrollTop = currentScrollTop);
+		});
 	}
 };
