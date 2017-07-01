f9548b8c9a51dd0a24403285458b15e264974717
Kingshuk Basak <kingshuk@fusioncharts.com>
2017-07-01 15:42:28 +0530

    Css separated in a different file. Initial search results are appearing

--- app.js
+++ app.js
@@ -6,24 +6,46 @@ function init () {
 		manageScroll();
 	});
 
-	var storage_title = [], 
-		storage_date = [],
+	var storage_title, 
+		storage_date,
 		scrollElm = [],
-		nextPageToken = "";
+		nextPageToken = '',
+		updateGrid = function () {
+			var i,
+				len = scrollElm.length,
+				item,
+				data = storage_date[0];
+
+			$('#searchContent').css("visibility", "visible");
+			for (i = 0; i < len; i++) {
+				item = scrollElm[i];
+				item.find('.pic').attr('src', data[i].thumbnail);
+				item.find('.title').html(data[i].title);
+				item.find('.pic').html(data[i].publishedAt);
+			}
+
+			updateGrid = undefined;
+		};
 
 	function createScroll () {
 		var i,
-			limit = 7;
-		for (i = 0; i < limit; i++) {
-			scrollElm[i] = $('#info').clone().appendTo('#scroller');
+			limit = 5;
+			sample = $('#info');
+
+		scrollElm.push(sample);
+		for (i = 1; i < limit; i++) {
+			scrollElm[i] = sample.clone().appendTo('#scroller');
 		}
 	}
 
 	// Setting up the youtube API
 	function setup () {
-		gapi.client.setApiKey("AIzaSyByitsr6ZZ4webABObIce9tmArFlnIMBzw");
-	    gapi.client.load("youtube", "v3", function() {
-	        $('#search').on("click", function() {
+		$('#info')
+		gapi.client.setApiKey('AIzaSyByitsr6ZZ4webABObIce9tmArFlnIMBzw');
+	    gapi.client.load('youtube', 'v3', function() {
+	        $('#search').on('click', function() {
+	        	storage_title = [];
+	        	storage_date = [];
 				executeRequest(createRequest());
 			})
 	    });
@@ -31,11 +53,11 @@ function init () {
 
 	function createRequest () {
 		return gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#searchText").val()).replace(/%20/g, "+"),
+            part: 'snippet',
+            type: 'video',
+            q: encodeURIComponent($('#searchText').val()).replace(/%20/g, '+'),
             maxResults: 5,
-            order: "viewCount",
+            order: 'viewCount',
             pageToken : nextPageToken
        }); 
 	};
@@ -47,7 +69,8 @@ function init () {
 				i,
 				obj,
 				len,
-				tempArr = [],
+				tempArrDate = [],
+				tempArrTitle = [],
 				snippet;
 
 			nextPageToken = response.nextPageToken;
@@ -58,14 +81,19 @@ function init () {
 					publishedAt : new Date(snippet.publishedAt),
 					thumbnail : snippet.thumbnails.default.url
 				}
-				tempArr.push(obj);
+				tempArrDate.push(obj);
+				tempArrTitle.push(obj);
 			}
 
-			tempArr.sort(function(a, b) {
+			storage_date.push(tempArrDate.sort(function(a, b) {
 				return a.publishedAt < b.publishedAt;
-			});
+			}));
 
-			console.log(tempArr)
+			storage_title.push(tempArrTitle.sort(function(a, b) {
+				return a.title < b.title;
+			}));
+
+			updateGrid && updateGrid();
        	});
 	};
 
