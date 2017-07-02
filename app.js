edec5caad93937b5c196b4d7106fedb4aafe2e57
Kingshuk Basak <kingshuk@fusioncharts.com>
2017-07-02 22:31:22 +0530

    Code comments added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 function init () {
 	$(function() {
-		setOrderingUI();
+		// Entry point of the application
 		setupYoutubeAPI();
+		setOrderingUI();
 		manageScroll();
-		createScrollableElem();
+		addInitialBlocks();
 	});
 
+	// List of varibles used across all the functions
 	var storage,
 		scrollElm = [],
 		nextPageToken,
@@ -22,18 +24,28 @@ function init () {
 		minContentHeight,
 		luckySearch;
 
+	/**
+	 * Function which is called as a callback after the completion of request of
+	 * youtube API
+	 */
 	function optionalGridUpdte () {
 		if (queuedStart || queuedEnd) {
-			updateGrid(queuedStart, queuedEnd + maxVisibleItems) && (lastDrawIndex += maxVisibleItems);
+			updateGrid(queuedStart, queuedEnd + maxVisibleItems);
 			queuedEnd = queuedStart = 0;
 		}
 	}
 
+	/**
+	 * Function to define the click functionality on the up and down arrows
+	 * for ordering of the displayed data
+	 */
 	function setOrderingUI () {
 		$('.glyphicon').on('click', function () {
 			var clickedDom = this;
+			// Iterating through all the elemets under the glyphicon class
 			$('.glyphicon').each(function() {
 				var currentItem = $(this);
+				// When the concerened elemnet is the element that is clicked
 				if (clickedDom === this) {
 					currentItem.hasClass('active') ?
 						currentItem.toggleClass('glyphicon-chevron-up glyphicon-chevron-down') :
@@ -44,12 +56,16 @@ function init () {
 					handleDataToggling();
 				}
 				else {
-					currentItem.removeClass('active glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+					currentItem.removeClass('active glyphicon-chevron-up')
+						.addClass('glyphicon-chevron-down');
 				}
 			});
 		});
 	}
 
+	/**
+	 * Function to manage data ordering and sorting
+	 */
 	function handleDataToggling () {
 		var len,
 			i,
@@ -58,17 +74,20 @@ function init () {
 			endIndex,
 			item;
 
+		// If the key for data display is changed then just update the anchor blocks 
+		// which by default happens in descending order.
 		if (prevStorageKey !== storageKey) {
-			updateGrid(0, scrollElm.length, true);
+			updateGrid(0, scrollElm.length);
 		}
+		// The flow enters this block when the order of the same key against which the
+		// elemnts are displayed is changed.
 		else {
 			len = scrollElm.length;
 			numOfPages = Math.floor(len / maxResult);
 
 			endIndex = lastDrawIndex + (maxResult - lastDrawIndex % maxResult);
 			// Drawing all the scroll element of the current page
-			updateGrid(lastDrawIndex, endIndex) &&
-				(lastDrawIndex += endIndex);
+			updateGrid(lastDrawIndex, endIndex);
 
 			function helperFN (i, j) {
 				index = maxResult * j + i;
@@ -92,16 +111,23 @@ function init () {
 		prevStorageKey = storageKey;
 	}
 
-	function createScrollableElem () {
+	/**
+	 * Function for creating the initial scrollable anchor blocks.
+	 */
+	function addInitialBlocks () {
 		var i,
 			sample = $('#info');
+			sample.hide();
 
-		scrollElm.push(sample);
-		for (i = 1; i < lastDrawIndex; i++) {
+		// Creating 2 extra blocks than the calculated maximum visible blocks
+		for (i = 0; i < maxVisibleItems + 2; i++) {
 			scrollElm[i] = sample.clone(true);
 		}
 	}
 
+	/**
+	 * Function to reset all the paramters every time a new query is searched
+	 */
 	function reset () {
 		var scrollerHeight = parseInt($('#scroller').css('max-height')),
 			i,
@@ -109,7 +135,7 @@ function init () {
 
 		minContentHeight = parseInt($('#info').css('min-height'));
 		maxVisibleItems = Math.ceil(scrollerHeight / minContentHeight) + 2;
-		lastDrawIndex = maxVisibleItems + 2;
+		lastDrawIndex = 0;
 		prevScrollTop = 0;
 		storage = {
 			storageTitle : [],
@@ -121,11 +147,13 @@ function init () {
     	queuedEnd = 0;
     	order = undefined;
     	
+    	// Setting the toggling class properly
     	$('#storageDate').addClass('active glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
     	$('#storageTitle').addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up active');
 
     	prevStorageKey = storageKey = $('#storageDate').hasClass('active') ? 'storageDate' : 'storageTitle';
 
+    	$('#iframe').attr('src','');
 
     	// Hiding all the previous search results
     	for (i = 0; i < len; i++) {
@@ -133,14 +161,16 @@ function init () {
     	}
 	}
 
-	// Setting up the youtube API
+	/**
+	 * Function to instantiate the youtube API and add eventListeners to the page.
+	 */
 	function setupYoutubeAPI () {
 		gapi.client.setApiKey('AIzaSyByitsr6ZZ4webABObIce9tmArFlnIMBzw');
 	    gapi.client.load('youtube', 'v3', function() {
 	        $('#search').on('click', function() {
 	        	reset();
 				executeRequest(createRequest());
-				updateGrid(0, lastDrawIndex);
+				updateGrid(0, maxVisibleItems + 2);
 			});
 
 	        $('a').on('click', function(e) {
@@ -155,6 +185,9 @@ function init () {
 	    });
 	}
 
+	/**
+	 * Function to create the youtube API request object where the result is ordered by viewCount
+	 */
 	function createRequest () {
 		return gapi.client.youtube.search.list({
             part: 'snippet',
@@ -166,6 +199,11 @@ function init () {
        }); 
 	}
 
+	/**
+	 * Function for sorting an array of objects based on a string
+	 * @param  {Object} a An object of the array
+	 * @param  {Object} b An object of the array
+	 */
 	function compare (a,b) {
 		if (a.title < b.title) {
 			return 1;
@@ -176,6 +214,10 @@ function init () {
 		return 0;
 	}
 
+	/**
+	 * Function to execute the previously formed youtube request object
+	 * @param  {Object} request The youtube API request object
+	 */
 	function executeRequest (request) {
 		request.execute(function (response) {
 			var result = response.result,
@@ -188,7 +230,10 @@ function init () {
 				tempArrTitle = [],
 				snippet;
 
+			// Storring the nextPageToken token for future query.
 			nextPageToken = response.nextPageToken;
+
+			// Parsing the result obtained into usable format
 			for (i = 0, len = items.length; i < len; i ++) {
 				snippet = items[i].snippet;
 				date = new Date(snippet.publishedAt);
@@ -203,21 +248,30 @@ function init () {
 				tempArrTitle.push(obj);
 			}
 
+			// Sorting the stored data as per the date the video is published
 			storage.storageDate.push(tempArrDate.sort(function (a, b) {
 				return b.publishedAt - a.publishedAt;
 			}));
+
+			// Sorting the stored data as per the title of the video
 			storage.storageTitle.push(tempArrTitle.sort(compare));
 
 			optionalGridUpdte();
 
+			// If 'i'm lucky' button is pressed, following code is executed.
 			if (luckySearch) {
-				$('iframe')[0].src = 'https://www.youtube.com/embed/' + $(scrollElm[0]).data('videoId') +
+				$('#iframe')[0].src = 'https://www.youtube.com/embed/' + $(scrollElm[0]).data('videoId') +
 					'?autoplay=1';
 				luckySearch = false;
 			}
        	});
 	}
 
+	/**
+	 * Fucntion to continuously expand the grid on demand by adding new clickable anchor elements
+	 * @param  {Number} start Index from where clickable elemets has to be added.
+	 * @param  {Number} end   Index upto which clickable elemets has to be added.
+	 */
 	function updateGrid (start, end) {
 		var i,
 			item,
@@ -239,8 +293,8 @@ function init () {
 				data = storage[storageKey][currentPageNumber];
 			}
 
-			// Response from youtube API is yet to be received so storing relevant data and recalling this function
-			// on callbak. 	
+			// Response from youtube API is yet to be received so storing relevant data
+			// which is used during the callback execution of youtube API
 			if (!(data && data[index])) {
 				queuedEnd = end;
 				queuedStart = start;
@@ -252,6 +306,8 @@ function init () {
 			if (!(item = scrollElm[i])) {
 				item = scrollElm[i] = sample.clone(true);
 			}
+
+			// Setting all the relevant properties to the clickable blocks
 			item.appendTo('#scroller');
 			item.data('videoId', datum.videoId);
 			item.show();
@@ -264,6 +320,7 @@ function init () {
 			if (index === 0) {
 				requestFlag = true;
 			}
+			lastDrawIndex++;
 		}
 
 		// if requestFlag is set and 10% of the previous result has been queried then make a new query
@@ -271,9 +328,11 @@ function init () {
 			requestFlag = false;
 			executeRequest(createRequest());
 		}
-		return true;
 	}
 
+	/**
+	 * Function to update the visuals when the grid is scrolled.
+	 */
 	function manageScroll () {
 		var scroller = $('#scroller'),
 			elementScrolled,
@@ -283,8 +342,9 @@ function init () {
 			currentScrollTop = $(this).scrollTop();
 			if (currentScrollTop > prevScrollTop) {
 				elementScrolled = Math.floor((currentScrollTop - prevScrollTop) / minContentHeight);
+				// If a complete block is scrolled up only then call the updateGrid function
 				if (elementScrolled) {
-					updateGrid(lastDrawIndex, lastDrawIndex + 1) && lastDrawIndex++;
+					updateGrid(lastDrawIndex, lastDrawIndex + 1);
 					prevScrollTop = currentScrollTop;
 				}
 			}
